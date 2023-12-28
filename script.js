346c3e4d6d27c5d9ea62cd1364df6a6c284430ec
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-12-27 21:05:57 -0500

    added milliseconds to timer as well styled it to liking.

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 let startTime;
-let stopWatchInterval;
-let elapsedPausedTime = 0
-const startButton = document.getElementById("startStopWatch")
-const stopButton = document.getElementById("stopStopWatch")
-const resetButton = document.getElementById("resetStopWatch")
+let elapsedPausedTime = 0;
+let animationFrameId;
+const startButton = document.getElementById("startStopWatch");
+const stopButton = document.getElementById("stopStopWatch");
+const resetButton = document.getElementById("resetStopWatch");
 
 startButton.addEventListener("click", () => {
-        if(!stopWatchInterval){
-            startTime = new Date().getTime() - elapsedPausedTime;
-            stopWatchInterval = setInterval(updateStopwatch, 1000);
-        }
-})
+    if (!animationFrameId) {
+        startTime = new Date().getTime() - elapsedPausedTime;
+        requestAnimationFrame(updateStopwatch);
+    }
+});
 
 stopButton.addEventListener("click", () => {
-    clearInterval(stopWatchInterval);
+    cancelAnimationFrame(animationFrameId);
     elapsedPausedTime = new Date().getTime() - startTime;
-    stopWatchInterval = null
-})
+    animationFrameId = null;
+});
 
 resetButton.addEventListener("click", () => {
-    clearInterval(stopWatchInterval);
-    elapsedPausedTime = new Date().getTime() - startTime;
-    stopWatchInterval = null
-    elapsedPausedTime = 0
-    document.getElementById("stopwatch").innerHTML = "00:00:00";
-})
+    cancelAnimationFrame(animationFrameId);
+    elapsedPausedTime = 0;
+    document.getElementById("stopwatch").innerHTML = "00:00:00 000";
+});
 
 function updateStopwatch() {
     let currentTime = new Date().getTime();
     let elapsedTime = currentTime - startTime;
+    let milliseconds = elapsedTime % 1000;
     let seconds = Math.floor(elapsedTime / 1000) % 60;
     let minutes = Math.floor(elapsedTime / 1000 / 60) % 60;
     let hours = Math.floor(elapsedTime / 1000 / 60 / 60);
-    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+    let displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds) + " " + pad(milliseconds, 3);
     document.getElementById("stopwatch").innerHTML = displayTime;
-  }
-  
-  function pad(number) {
-    return (number < 10 ? "0" : "") + number;
-  }
+    animationFrameId = requestAnimationFrame(updateStopwatch);
+}
+
+function pad(number, width = 2) {
+    return (new Array(width).join('0') + number).slice(-width);
+}
